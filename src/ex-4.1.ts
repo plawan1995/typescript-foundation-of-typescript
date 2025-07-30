7d2c094fd19f073346115a1bab676c805df0fc05
Chanisara <chanisara.sra@gmail.com>
2025-07-30 15:45:10 +0700

    feat : chom ex 1 - 4.2 Implement basic arithmetic operations in calculate function, sum even numbers in array, enhance order processing with type definitions and discount calculations, and add type checks for user age and data processing functions.

--- src/ex-4.1.ts
+++ src/ex-4.1.ts
@@ -5,10 +5,17 @@ type User = {
 };
 
 function isAdult(user: User): boolean {
+  //ส่วนที่แก้ไข
+  if (user.age === undefined) {
+    return false;
+  }
   return user.age >= 18;
 }
 
 const result = isAdult({ id: "u01", name: "John" });
 console.log(result); // ควรได้ false
 
-// Error ที่เจอคือ
+// Error ที่เจอคือ age กำหนดให้อาจจะไม่มีค่า(undefined)ก็ได้
+// ในกรณีที่ age เป็น undefined จะทำให้เกิดการเปรียบเทียบ undefined กับ number (undefined >= 18)
+// TS จึงมองว่าไม่ปลอดภัย ทำให้เกิด error
+// การแก้ไขคือเพิ่มการเปรียบเทียบ เพื่อตรวจสอบ age ก่อนว่าเป็น undefined หรือไม่
